f4d51a0511dcee3d5c17e9a8cd0201b8a0dcd04d
MUCANDA <mucanda@discente.ufg.br>
2024-09-27 16:26:49 -0300

    arquivo novo tabela

--- sokoban.js
+++ sokoban.js
@@ -87,33 +87,7 @@ function createGameElement(elementName, className, parentNode) {
 
    return element;
 }
-function buildGameBoard(numRows, numCols) {
-   const game = document.getElementById("jogo");
-   const board = createGameElement('div', 'tabela', game);
-   const positionOfPieces = createGameElement('div', 'jogador', board);
 
-
-   for (let k = 0; k < numRows; k++) {
-      const linha = createGameElement('div', 'linha', board);
-
-
-
-      for (let i = 0; i < numCols; i++) {
-         const celula = createGameElement('div', 'celula', linha);
-         const char = boardMap[k][i];
-
-
-         if (char === '#') celula.classList.add('wall');
-         if (char === 'G') celula.classList.add('goal');
-         if (char === 'p') celula.classList.add('box');
-         if (char === 'p') {
-            player = new player(k, i);
-         }
-      }
-   }
-
-   return positionOfPieces;
-}
 
 
 
